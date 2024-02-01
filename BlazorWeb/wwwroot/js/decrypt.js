bc82c6988804a48e7110047c4016e3b63a9055d6
Đô Nguyễn Thành <thanhdo@atlantic.edu.vn>
2024-02-01 22:46:25 +0700

    Clean code

--- BlazorWeb/wwwroot/js/decrypt.js
+++ BlazorWeb/wwwroot/js/decrypt.js
@@ -1,112 +1,11 @@
 ﻿window.methods = {
-    decryptAES: async function ({ key, iv, encryptedString }) {
-        const crypto = CryptoJS;
 
-        const ciphertextWA = crypto.enc.Base64.parse(encryptedString);
-        const keyWA = crypto.enc.Utf8.parse(key);
-        const ivWA = crypto.enc.Utf8.parse(iv);
-        const ciphertextCP = { ciphertext: ciphertextWA };
-
-        const decrypted = await crypto.AES.decrypt(
-            ciphertextCP,
-            keyWA,
-            { iv: ivWA }
-        );
-        console.log('decrypted', decrypted);
-
-        // Chuyển đổi kết quả thành UTF-8
-        const result = decrypted.toString(crypto.enc.Utf8);
-        console.log('result', result);
-
-        return JSON.parse(result);
-    },
-
-    decompressByteStringToString: async function ({ key, iv, byteString }) {
-        var binaryString = atob(byteString);
-        var bytes = new Uint8Array(binaryString.length);
-        for (var i = 0; i < binaryString.length; i++) {
-            bytes[i] = binaryString.charCodeAt(i);
-        }
-        var data = msgpack.decode(bytes);
-        return data;
-    },
 
     decompressByteArrayToString: async function ({ key, iv, byteArray }) {
-        // Tạo ArrayBuffer từ chuỗi byte
-        //const byteArray = byteString.split(',').map(Number);
-        //debugger;
 
         var result = msgpack.decode(byteArray);
         const jsonData = JSON.parse(result);
 
-        //let buffer = Buffer.from(byteArray);
-        //var result = Buffer.from(byteArray.buffer).toString();
-        //var result = utf8ArrayToStrfunction(byteArray);
-        //var result = (new TextDecoder()).decode(byteArray);
-        //var result = byteArray.map(b => String.fromCharCode(b)).join("");;
-
-        //var out, i, a, c;
-        //var char2, char3;
-        //out = "";
-        //a = byteArray.length;
-        //i = 0;
-        //while (i < a) {
-        //    c = byteArray[i++];
-        //    switch (c >> 4) {
-        //        case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
-        //            // 0xxxxxxx
-        //            out += String.fromCharCode(c);
-        //            break;
-        //        case 12: case 13:
-        //            // 110x xxxx   10xx xxxx
-        //            char2 = byteArray[i++];
-        //            out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
-        //            break;
-        //        case 14:
-        //            // 1110 xxxx  10xx xxxx  10xx xxxx
-        //            char2 = byteArray[i++];
-        //            char3 = byteArray[i++];
-        //            out += String.fromCharCode(((c & 0x0F) << 12) |
-        //                ((char2 & 0x3F) << 6) |
-        //                ((char3 & 0x3F) << 0));
-        //            break;
-        //    }
-        //}
-
-        //// Chuyển đổi văn bản thành đối tượng JSON
-        //const jsonData = JSON.parse(out);
-
         return jsonData;
     },
-
-    utf8ArrayToStrfunction: function (array) {
-        var out, i, a, c;
-        var char2, char3;
-        out = "";
-        a = array.length;
-        i = 0;
-        while (i < a) {
-            c = array[i++];
-            switch (c >> 4) {
-                case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
-                    // 0xxxxxxx
-                    out += String.fromCharCode(c);
-                    break;
-                case 12: case 13:
-                    // 110x xxxx   10xx xxxx
-                    char2 = array[i++];
-                    out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
-                    break;
-                case 14:
-                    // 1110 xxxx  10xx xxxx  10xx xxxx
-                    char2 = array[i++];
-                    char3 = array[i++];
-                    out += String.fromCharCode(((c & 0x0F) << 12) |
-                        ((char2 & 0x3F) << 6) |
-                        ((char3 & 0x3F) << 0));
-                    break;
-            }
-        }
-        return out;
-    }
 }
--- Server/WeatherHub.cs
+++ Server/WeatherHub.cs
@@ -18,121 +18,16 @@ namespace Server
         private const string _key = "SECRETKEYUSEDSYMMETRICENCRYPTION";
         private const string _iv = "19xj02lop5n9t8aw";
 
-        //private readonly List<WeatherForecast> _weatherData;
-        //public WeatherHub(WeatherForecastBinary weatherForecastBinary)
-        //{
-        //    _weatherData = weatherForecastBinary.WeatherForecasts;
-        //}
-
         public async Task GetWeatherData()
         {
             // Đọc dữ liệu từ file JSON và đăng ký nó như một singleton service
             var dataBinary = File.ReadAllText("WeatherData.json");
             var weatherData = JsonConvert.DeserializeObject<List<WeatherForecast>>(dataBinary);
-            var weatherForecastBinary = new WeatherForecastBinary();
+
             string jsonData = JsonConvert.SerializeObject(weatherData);
             var messagePackData = MessagePackSerializer.Serialize(jsonData);
 
-            //MemoryStream ms = new MemoryStream();
-            //using (BsonWriter writer = new BsonWriter(ms))
-            //{
-            //    JsonSerializer serializer = new JsonSerializer();
-            //    serializer.Serialize(writer, weatherData);
-            //}
-            //string data = Convert.ToBase64String(ms.ToArray());
-
-            //JsonSerializer jsonSerializer = new JsonSerializer();
-            //MemoryStream objBsonMemoryStream = new MemoryStream();
-            //BsonWriter bsonWriterObject = new BsonWriter(objBsonMemoryStream);
-            //jsonSerializer.Serialize(bsonWriterObject, weatherData);
-
-            weatherForecastBinary.EndDateEncrypted = DateTime.UtcNow;
-            weatherForecastBinary.EncryptedDataByte = messagePackData;
-            await Clients.All.SendAsync("GetWeatherData", weatherForecastBinary);
-        }
-
-        //public async Task GetWeatherData()
-        //{
-        //    // Đọc dữ liệu từ file JSON và đăng ký nó như một singleton service
-        //    var dataBinary = File.ReadAllText("WeatherData.json");
-        //    var weatherData = JsonSerializer.Deserialize<List<WeatherForecast>>(dataBinary);
-
-        //    var data = new WeatherForecastBinary();
-        //    string jsonData = JsonSerializer.Serialize(weatherData);
-        //    string encryptedData = EncryptString(jsonData, _key); // Hàm mã hóa của bạn
-
-        //    data.EndDateEncrypted = DateTime.UtcNow;
-        //    data.EncryptedData = encryptedData;
-        //    await Clients.All.SendAsync("GetWeatherData", data);
-        //}
-
-        //public async Task UpdateWeatherData(string encryptedData)
-        //{
-        //    string decryptedData = EncryptString(encryptedData, _key); // Hàm giải mã của bạn
-        //    var updatedData = JsonSerializer.Deserialize<List<WeatherForecast>>(decryptedData);
-
-        //    await Clients.All.SendAsync("ReceiveWeatherData", updatedData);
-        //}
-
-        public static string EncryptString(string text, string keyString)
-        {
-            byte[] iv = Encoding.ASCII.GetBytes(_iv);
-            byte[] array;
-
-            using (var aes = Aes.Create())
-            {
-                aes.Key = Encoding.UTF8.GetBytes(keyString);
-                aes.IV = iv;
-
-                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
-
-                using (MemoryStream memoryStream = new MemoryStream())
-                {
-                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
-                    {
-                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
-                        {
-                            streamWriter.Write(text);
-                        }
-
-                        array = memoryStream.ToArray();
-                    }
-                }
-            }
-
-            return Convert.ToBase64String(array);
-        }
-
-        public static string DecryptString(string cipherText, string keyString)
-        {
-            var fullCipher = Convert.FromBase64String(cipherText);
-
-            var iv = new byte[16];
-            var cipher = new byte[16];
-
-            Buffer.BlockCopy(fullCipher, 0, iv, 0, iv.Length);
-            Buffer.BlockCopy(fullCipher, iv.Length, cipher, 0, iv.Length);
-            var key = Encoding.UTF8.GetBytes(keyString);
-
-            using (var aesAlg = Aes.Create())
-            {
-                using (var decryptor = aesAlg.CreateDecryptor(key, iv))
-                {
-                    string result;
-                    using (var msDecrypt = new MemoryStream(cipher))
-                    {
-                        using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
-                        {
-                            using (var srDecrypt = new StreamReader(csDecrypt))
-                            {
-                                result = srDecrypt.ReadToEnd();
-                            }
-                        }
-                    }
-
-                    return result;
-                }
-            }
+            await Clients.All.SendAsync("GetWeatherData", messagePackData);
         }
     }
 }
